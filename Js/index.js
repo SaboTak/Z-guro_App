ae4b08dc24e2fbdfa854a48312ef1bebeb352af1
SaboTak <guideon.slayer@gmail.com>
2020-11-18 14:59:53 -0500

    Arreglos

--- Js/index.js
+++ Js/index.js
@@ -1,25 +1,8 @@
+'use strict'
 
- /*Funcion de Capturar, Almacenar datos y Limpiar campos*/
- $(document).ready(function(){    
-    $('#boton-guardar').click(function(){        
-        /*Captura de datos escrito en los inputs*/        
-        var sintomas = document.getElementById("sintomas").value;
-        var eps = document.getElementById("eps").value;
-        /*Guardando los datos en el LocalStorage*/
-        sessionStorage.setItem("Sintomas", sintomas);
-        sessionStorage.setItem("Eps", eps);
+window.addEventListener('load',function(){
+    //Elemento cargador
+    console.log("dom cargado");
 
-    });   
-
-
-    /* 
-    Funcion Cargar y Mostrar datos
-$(document).ready(function(){    
-    $('#boton-cargar').click(function(){                       
-        /*Obtener datos almacenados
-        var sintomas = sessionStorage.getItem("Sintomas");
-        var eps = sessionStorage.getItem("Eps");
-
-    });   
-    }   );
-    */
+    // index
+})
